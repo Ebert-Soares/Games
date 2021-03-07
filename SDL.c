ecb5d1cb5190725e983a9afa385284f99fc496cc
Ebert Soares <59069184+Ebert-Soares@users.noreply.github.com>
2021-03-07 19:57:56 -0300

    Still not working

--- SDL.c
+++ SDL.c
@@ -5,31 +5,28 @@
 SDL_Window* window = 0;
 SDL_Renderer* render = 0;
 typedef enum { false, true } bool;
-SDL_Texture* texture;
-SDL_Rect fRetangle;
-SDL_Rect dRetangle;
+SDL_Texture* m_pTexture; // the new SDL_Texture variable
+SDL_Rect m_sourceRectangle; // the first rectangle
+SDL_Rect m_destinationRectangle; // another rectangle
 
 
-int textureDef(){
-  SDL_Surface* surface = SDL_LoadBMP("assets/tiger.bmp");
-  texture = SDL_CreateTextureFromSurface(render, surface);
-  SDL_FreeSurface(surface);
-  SDL_QueryTexture(texture, NULL, NULL, &fRetangle.w, &fRetangle.h);
-
-  dRetangle.x = fRetangle.x = 0;
-  dRetangle.y = fRetangle.y = 0;
-  dRetangle.w = fRetangle.w;
-  dRetangle.h = fRetangle.h;
-
-}
 
 int init(const char* title, int xpos, int ypos, int height, int width, int flags){
-  if(SDL_Init(SDL_INIT_EVERYTHING) >= 0){
   window = SDL_CreateWindow(title, xpos, ypos, height, width, flags);
+
+
+
 
     if(window != 0)
   {
        render = SDL_CreateRenderer(window, -1, 0);
+       SDL_Surface* pTempSurface = SDL_LoadBMP("assets/rider.bmp");
+       m_pTexture = SDL_CreateTextureFromSurface(render, pTempSurface);
+       SDL_QueryTexture(m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);
+       m_destinationRectangle.x = m_sourceRectangle.x = 0;
+       m_destinationRectangle.y = m_sourceRectangle.y = 0;
+       m_destinationRectangle.w = m_sourceRectangle.w;
+       m_destinationRectangle.h = m_sourceRectangle.h;
 
   }
      else
@@ -37,15 +34,15 @@ int init(const char* title, int xpos, int ypos, int height, int width, int flags
       return 1;}
 
   }
-}
+
 
 void renderize()
 {
 
  SDL_SetRenderDrawColor(render, 0, 0, 0, 255);
- SDL_RenderClear(render);
- SDL_RenderCopy(render, texture, &fRetangle, &dRetangle);
- SDL_RenderPresent(render);
+ SDL_RenderCopy(render, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
+  SDL_RenderClear(render);
+  SDL_RenderPresent(render);
 }
 
 void clean(){
@@ -57,11 +54,8 @@ void clean(){
 }
 
 int main(int argc, char *argv[]){
+  init("SDL game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
 
-  textureDef();
-
-
-  init("SDL game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, fRetangle.h, fRetangle.w, SDL_WINDOW_SHOWN);
   renderize();
   SDL_Delay(1000);
   clean();
