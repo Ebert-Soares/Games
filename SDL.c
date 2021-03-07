c2ef42207794570077989fec4f8d074bd492c5f3
Ebert Soares <59069184+Ebert-Soares@users.noreply.github.com>
2021-03-07 19:29:14 -0300

    Strange bug

--- SDL.c
+++ SDL.c
@@ -5,11 +5,28 @@
 SDL_Window* window = 0;
 SDL_Renderer* render = 0;
 typedef enum { false, true } bool;
+SDL_Texture* texture;
+SDL_Rect fRetangle;
+SDL_Rect dRetangle;
 
+
+int textureDef(){
+  SDL_Surface* surface = SDL_LoadBMP("assets/tiger.bmp");
+  texture = SDL_CreateTextureFromSurface(render, surface);
+  SDL_FreeSurface(surface);
+  SDL_QueryTexture(texture, NULL, NULL, &fRetangle.w, &fRetangle.h);
+
+  dRetangle.x = fRetangle.x = 0;
+  dRetangle.y = fRetangle.y = 0;
+  dRetangle.w = fRetangle.w;
+  dRetangle.h = fRetangle.h;
+
+}
 
 int init(const char* title, int xpos, int ypos, int height, int width, int flags){
   if(SDL_Init(SDL_INIT_EVERYTHING) >= 0){
   window = SDL_CreateWindow(title, xpos, ypos, height, width, flags);
+
     if(window != 0)
   {
        render = SDL_CreateRenderer(window, -1, 0);
@@ -27,6 +44,7 @@ void renderize()
 
  SDL_SetRenderDrawColor(render, 0, 0, 0, 255);
  SDL_RenderClear(render);
+ SDL_RenderCopy(render, texture, &fRetangle, &dRetangle);
  SDL_RenderPresent(render);
 }
 
@@ -39,8 +57,11 @@ void clean(){
 }
 
 int main(int argc, char *argv[]){
-  init("SDL game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
 
+  textureDef();
+
+
+  init("SDL game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, fRetangle.h, fRetangle.w, SDL_WINDOW_SHOWN);
   renderize();
   SDL_Delay(1000);
   clean();
