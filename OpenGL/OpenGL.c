d9b8a8f60ad6fcbe3ba72e4475e16860b4b9e028
Ebert Soares <59069184+Ebert-Soares@users.noreply.github.com>
2021-04-19 15:53:55 -0300

    d

--- OpenGL/OpenGL.c
+++ OpenGL/OpenGL.c
@@ -1,8 +1,31 @@
-#include <GL/gl.h>
+#include <glad/glad.h>
 #include "GLFW/include/glfw3.h"
+#include <stdio.h>
+
 
 
 
 int main(){
- return 0;
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_FALSE);
+
+
+  GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+
+  glfwMakeContextCurrent(window);
+
+
+  if (gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+  {
+    printf("error");
+    return 1;
+}
+  glViewport(0, 0, 800, 600);
+
+
+  return 0;
+
 }
--- OpenGL/make.bat
+++ OpenGL/make.bat
@@ -1,2 +1,2 @@
 
-gcc -I C:\MinGW\include\GLFW\include OpenGL.c -L C:\MinGW\include\GLFW\lib -lglfw3dll -lopengl32 -lmingw32 -lgdi32 -luser32 -lkernel32 -o opengl.exe
+gcc -I C:\MinGW\include\GLFW\include C:\Users\ebert\Documents\GitHub\Games\OpenGL\glad.c OpenGL.c -L C:\MinGW\include\GLFW\lib -lglfw3dll -lopengl32 -lmingw32 -lgdi32 -luser32 -lkernel32  -o opengl.exe
