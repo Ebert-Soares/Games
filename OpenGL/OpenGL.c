b151dea4788547dc68f7cfa93dbd68501b3e8d1e
Ebert Soares <59069184+Ebert-Soares@users.noreply.github.com>
2021-04-19 15:06:07 -0300

    aa

---
+++ OpenGL/OpenGL.c
@@ -0,0 +1,8 @@
+#include <GL/gl.h>
+#include "GLFW/include/glfw3.h"
+
+
+
+int main(){
+ return 0;
+}
---
+++ OpenGL/make.bat
@@ -0,0 +1,2 @@
+
+gcc -I C:\MinGW\include\GLFW\include OpenGL.c -L C:\MinGW\include\GLFW\lib -lglfw3dll -lopengl32 -lmingw32 -lgdi32 -luser32 -lkernel32 -o opengl.exe
